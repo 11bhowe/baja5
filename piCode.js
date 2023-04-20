b3da7a00ff3b9d7e560fca971dce870c9f71a576
11bhowe <81282583+11bhowe@users.noreply.github.com>
2023-04-20 12:53:15 -0500

    Create piCode.js

---
+++ piCode.js
@@ -0,0 +1,256 @@
+const { createBluetooth } = require( 'node-ble' );
+
+const ARDUINO_BLUETOOTH_ADDR1 = '4E:4F:19:3B:D9:BE';
+const ARDUINO_BLUETOOTH_ADDR2 = 'FE:EF:08:21:BD:2D';
+
+const UART_SERVICE_UUID      = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E';
+const CMD_CHARACTERISTIC_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E';
+
+const EES_SERVICE_UUID       = '0000181a-0000-1000-8000-00805f9b34fb';
+const S1_CHARACTERISTIC_UUID = '00002a56-0000-1000-8000-00805f9b34fb';
+const S2_CHARACTERISTIC_UUID = '00002a57-0000-1000-8000-00805f9b34fb';
+
+var firebase = require('firebase/app');
+const {getDatabase, ref, onValue, set, update, get} = require('firebase/database');
+const firebaseConfig = {
+    apiKey: "AIzaSyA_LepERT3pgP_ZV3A4918nauHB6eWlXjQ",
+    authDomain: "gas-sensor-4ff34.firebaseapp.com",
+    projectId: "gas-sensor-4ff34",
+    storageBucket: "gas-sensor-4ff34.appspot.com",
+    messagingSenderId: "505655438829",
+    appId: "1:505655438829:web:ce6a1752bca49bd46a4be4",
+    // measurementId: "G-RCBJSXDZP2",
+    databaseURL: "https://gas-sensor-4ff34-default-rtdb.firebaseio.com/"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = getDatabase();
+
+const numDevices = 1;
+var vent1 = false;
+var vent2 = false;
+
+async function main( )
+{
+    var dict = {};
+
+    // Reference the BLE adapter and begin device discovery...
+    const { bluetooth, destroy } = createBluetooth();
+    const adapter = await bluetooth.defaultAdapter();
+    const discovery =  await adapter.startDiscovery();
+    console.log( 'discovering...' );
+
+    // Attempt to connect to the device with specified BT address
+    const device1 = await adapter.waitDevice( ARDUINO_BLUETOOTH_ADDR1.toUpperCase() );
+    console.log( 'found device 1. attempting connection...' );
+    await device1.connect();
+    console.log( 'connected to device 1!' );
+
+    // Get references to the desired UART service and its characteristics
+    const gattServer1 = await device1.gatt();
+    const uartService1 = await gattServer1.getPrimaryService( UART_SERVICE_UUID.toLowerCase() );
+    const d1cmd = await uartService1.getCharacteristic( CMD_CHARACTERISTIC_UUID.toLowerCase() );
+    const eesService1 = await gattServer1.getPrimaryService(EES_SERVICE_UUID.toLowerCase());
+    const d1sensor1 = await eesService1.getCharacteristic(S1_CHARACTERISTIC_UUID.toLowerCase());
+    const d1sensor2 = await eesService1.getCharacteristic(S2_CHARACTERISTIC_UUID.toLowerCase());
+    console.log('UART and ESS initialized...');
+
+    // Register for notifications on the characteristics
+    await d1sensor1.startNotifications( );
+    await d1sensor2.startNotifications( );
+
+    // Callback for when data is received on characteristics
+    d1sensor1.on( 'valuechanged', buffer =>
+    {
+        let lsb = buffer[0];
+        let msb = buffer[1];
+        let val = (lsb + (msb << 8));
+        var d = val.toString().substring(1,2);
+        var a = val.toString().substring(2);
+        console.log('Device 1 Sensor 1:   digital - ' + d + '    analog - ' + a);
+        dict['d1s1a'] = parseInt(buffer.toString().substring(2));
+        dict['d1s1d'] = parseInt(buffer.toString().substring(1,2));
+
+        update(ref(database, '/device1/sensor1/'), {
+            analog: dict['d1s1a'],
+            digital: dict['d1s1d']
+        });
+    });
+    d1sensor2.on( 'valuechanged', buffer =>
+    {
+        let lsb = buffer[0];
+        let msb = buffer[1];
+        let val = (lsb + (msb << 8));
+        var d = val.toString().substring(1,2);
+        var a = val.toString().substring(2);
+        console.log('Device 1 Sensor 2:   digital - ' + d + '    analog - ' + a);
+        dict['d1s2a'] = parseInt(buffer.toString().substring(2));
+        dict['d1s2d'] = parseInt(buffer.toString().substring(1,2));
+
+        update(ref(database, '/device1/sensor2'), {
+            analog: dict['d1s2a'],
+            digital: dict['d1s2d']
+        });
+    });
+
+    onValue(ref(database, '/device1/sensor1/digital'), (snapshot) => {
+        const data = snapshot.val();
+        if (data == 0) {
+            update(ref(database, '/device1'), {
+                vent: true
+            });
+        }
+        if (data == 1) {
+            get(ref(database, '/device1/sensor2/digital')).then((snapshot1) => {
+                const d = snapshot1.val();
+                if (d == 1) {
+                    update(ref(database, '/device1'), {
+                        vent: false
+                    });
+                }
+            });
+        }
+    });
+    onValue(ref(database, '/device1/sensor2/digital'), (snapshot) => {
+        const data = snapshot.val();
+        if (data == 0) {
+            update(ref(database, '/device1'), {
+                vent: true
+            });
+        }
+        if (data == 1) {
+            get(ref(database, '/device1/sensor1/digital')).then((snapshot1) => {
+                const d = snapshot1.val();
+                if (d == '1') {
+                    update(ref(database, '/device1'), {
+                        vent: false
+                    });
+                }
+            });
+        }
+    });
+    onValue(ref(database, '/device1/vent'), (snapshot) => {
+        const data = snapshot.val();
+        if (data == true) {
+            d1cmd.writeValue(Buffer.from('VENT ON')).then(() =>
+            {
+                console.log('Sent: VENT ON');
+            });
+        } else if (data == false) {
+            d1cmd.writeValue(Buffer.from('VENT OFF')).then(() =>
+            {
+                console.log('Sent: VENT OFF');
+            });
+        }
+    });
+
+
+    if (numDevices == 2) {
+        // Attempt to connect to the device with specified BT address
+        const device2 = await adapter.waitDevice( ARDUINO_BLUETOOTH_ADDR2.toUpperCase() );
+        console.log( 'found device 2. attempting connection...' );
+        await device2.connect();
+        console.log( 'connected to device 2!' );
+
+        // Get references to the desired UART service and its characteristics
+        const gattServer2 = await device2.gatt();
+        const uartService2 = await gattServer2.getPrimaryService( UART_SERVICE_UUID.toLowerCase() );
+        const d2cmd = await uartService2.getCharacteristic( CMD_CHARACTERISTIC_UUID.toLowerCase() );
+        const eesService2 = await gattServer2.getPrimaryService(EES_SERVICE_UUID.toLowerCase());
+        const d2sensor1 = await eesService2.getCharacteristic(S1_CHARACTERISTIC_UUID.toLowerCase());
+        const d2sensor2 = await eesService2.getCharacteristic(S1_CHARACTERISTIC_UUID.toLowerCase());
+
+        // Register for notifications on the characteristics
+        await d2sensor1.startNotifications( );
+        await d2sensor2.startNotifications( );
+
+        // Callback for when data is received on characteristics
+        d2sensor1.on( 'valuechanged', buffer =>
+        {
+            let lsb = buffer[0];
+            let msb = buffer[1];
+            let val = (lsb + (msb << 8));
+            console.log('Device 2 Sensor 1: ' + val.toString());
+            dict['d2s1a'] = parseInt(buffer.toString().split(' ')[0]);
+            dict['d2s1d'] = parseInt(buffer.toString().split(' ')[1]);
+
+            update(ref(database, '/device2/sensor1'), {
+                analog: dict['d2s1a'],
+                digital: dict['d2s1d']
+            });
+        });
+        d2sensor2.on( 'valuechanged', buffer =>
+        {
+            let lsb = buffer[0];
+            let msb = buffer[1];
+            let val = (lsb + (msb << 8));
+            console.log('Device 2  Sensor 2: ' + val.toString());
+            dict['d2s2a'] = parseInt(buffer.toString().split(' ')[0]);
+            dict['d2s2d'] = parseInt(buffer.toString().split(' ')[1]);
+
+            update(ref(database, '/device2/sensor2'), {
+                analog: dict['d2s2a'],
+                digital: dict['d2s2d']
+            });
+        });
+
+        onValue(ref(database, '/device2/sensor1/digital'), (snapshot) => {
+            const data = snapshot.val();
+            if (data == 0) {
+                update(ref(database, '/device2'), {
+                    vent: true
+                });
+            }
+            if (data == 1) {
+                get(ref(database, '/device2/sensor2/digital')).then((snapshot1) => {
+                    const d = snapshot1.val();
+                    if (d == 1) {
+                        update(ref(database, '/device2'), {
+                            vent: false
+                        });
+                    }
+                });
+            }
+        });
+        onValue(ref(database, '/device2/sensor2/digital'), (snapshot) => {
+            const data = snapshot.val();
+            if (data == 0) {
+                update(ref(database, '/device2'), {
+                    vent: true
+                });
+            }
+            if (data == 1) {
+                get(ref(database, '/device2/sensor1/digital')).then((snapshot1) => {
+                    const d = snapshot1.val();
+                    if (d == 1) {
+                        update(ref(database, '/device2'), {
+                            vent: false
+                        });
+                    }
+                });
+            }
+        });
+        onValue(ref(database, '/device2/vent'), (snapshot) => {
+            const data = snapshot.val();
+            if (data == true) {
+                d2cmd.writeValue(Buffer.from('VENT ON')).then(() =>
+                {
+                    console.log('Sent: VENT ON');
+                });
+            } else if (data == false) {
+                d2cmd.writeValue(Buffer.from('VENT OFF')).then(() =>
+                {
+                    console.log('Sent: VENT OFF');
+                });
+            }
+        });
+    }
+}
+
+main().then((ret) =>
+{
+    if (ret) console.log( ret );
+}).catch((err) =>
+{
+    if (err) console.error( err );
+});

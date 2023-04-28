b01ed13e9f165e912f4fcdf77708cbdd4745b208
11bhowe <81282583+11bhowe@users.noreply.github.com>
2023-04-27 22:01:56 -0500

    Update nanoCode.ino

--- src/nanoCode/nanoCode.ino
+++ src/nanoCode/nanoCode.ino
@@ -1,28 +1,27 @@
 #include <ArduinoBLE.h>
 
-/*   We'll use the ArduinoBLE library to simulate a basic UART connection 
- *   following this UART service specification by Nordic Semiconductors. 
- *   More: https://learn.adafruit.com/introducing-adafruit-ble-bluetooth-low-energy-friend/uart-service
- */
 BLEService uartService("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
 BLEStringCharacteristic cmdChar("6E400002-B5A3-F393-E0A9-E50E24DCCA9E", BLEWrite, 20);
 
-/*  Create a Environmental Sensing Service (ESS) and a 
- *  characteristic for its temperature value.
- */
 BLEService essService("181A");
 BLEShortCharacteristic sensor1("2A56", BLERead | BLENotify );
 BLEShortCharacteristic sensor2("2A57", BLERead | BLENotify );
 
+int deviceID;
 int analog1;
 int digital1;
 int analog2;
 int digital2;
 
+int LEDpin = 12;
+int pins[][4] = { // A1 D1 A2 D2
+                    {6, 3, 7, 4},  // Device 1
+                    {0, 2, 1, 3}   // Device 2
+                };
+
+
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
 
   if (!BLE.begin()) {
     Serial.println("Starting BLE failed!");
@@ -30,7 +29,18 @@ void setup() {
       ;
   }
 
-  String deviceAddress = BLE.address();
+  // Determine which Arduino address for correct pin configuration
+  if (BLE.address() == "4e:4f:19:3b:d9:be") {
+    deviceID = 0;    
+  }
+  if (BLE.address() == "fe:ef:08:21:bd:2d") {
+    deviceID = 1;
+  }
+  
+  pinMode(pins[deviceID][1], INPUT);
+  pinMode(pins[deviceID][3], INPUT);
+  pinMode(LEDpin, OUTPUT);
+
   BLE.setLocalName("ArduinoBLE Gas Sensor");
   BLE.setAdvertisedService(uartService);
   essService.addCharacteristic(sensor1);
@@ -39,7 +49,7 @@ void setup() {
   BLE.addService(essService);
   BLE.addService(uartService);
   BLE.advertise();
-  Serial.println("Bluetooth device (" + deviceAddress + ") active, waiting for connections...");
+  Serial.println("Bluetooth device (" + BLE.address() + ") active, waiting for connections...");
 }
 
 void loop() {
@@ -51,8 +61,8 @@ void loop() {
 
     while (central.connected()) {
       // Read sensor 1
-      analog1 = analogRead(0);
-      digital1 = digitalRead(2);
+      analog1 = analogRead(pins[deviceID][0]);
+      digital1 = digitalRead(pins[deviceID][1]);
 
       String msg1 = "1" + String(digital1, DEC) + String(analog1, DEC);               // combine digital & analog into single message
       short short1 = (short) msg1.toInt();                                            // convert to short for transmission
@@ -61,8 +71,8 @@ void loop() {
       
 
       // Read sensor 2
-      analog2 = analogRead(1);  
-      digital2 = digitalRead(3);
+      analog2 = analogRead(pins[deviceID][2]);  
+      digital2 = digitalRead(pins[deviceID][3]);
 
       String msg2 = "1" + String(digital2, DEC) + String(analog2, DEC);
       short short2 = (short) msg2.toInt();
@@ -72,15 +82,21 @@ void loop() {
 
       // listen for VENT command from Pi
       if (cmdChar.written()) {
-        Serial.print("[Recv] ");
-        Serial.println(cmdChar.value());
+        String command = cmdChar.value();        
+        Serial.print("[PI COMMAND] ");
+        Serial.println(command);
+        if (command == "VENT ON") {
+          digitalWrite(LEDpin,HIGH);
+        } else {
+          digitalWrite(LEDpin,LOW);
+        }
       }
       
       delay(3000);
     }
 
-  //   // Serial.print("Disconnected from central: ");
-  //   // Serial.println(central.address());
+    // Serial.print("Disconnected from central: ");
+    // Serial.println(central.address());
   }
 }
 
